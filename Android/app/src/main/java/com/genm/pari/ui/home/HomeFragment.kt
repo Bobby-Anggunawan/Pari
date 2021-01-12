14295e0254852c48678ad8dc7dd95ac4c2450c0c
unknown <yeremia.blog@gmail.com>
2021-01-13 06:44:04 +0700

    re-commit(lupa di add)

--- Android/app/src/main/java/com/genm/pari/ui/home/HomeFragment.kt
+++ Android/app/src/main/java/com/genm/pari/ui/home/HomeFragment.kt
@@ -10,11 +10,25 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.genm.pari.MainActivity
 import com.genm.pari.R
+import com.genm.pari.adapter.ToolFragment_Adapter
+import com.genm.pari.ui.tool.ToolFragment
 
 class HomeFragment : Fragment() {
+
+  lateinit var myRecyclerView: RecyclerView
+  companion object{
+    var items = arrayListOf<ToolFragment_Adapter.ToolItem>(
+            ToolFragment_Adapter.ToolItem(R.drawable.ic_compas_black_24dp, "SOS"),
+            ToolFragment_Adapter.ToolItem(R.drawable.ic_compas_black_24dp, "Radar Ikan"),
+            ToolFragment_Adapter.ToolItem(R.drawable.ic_compas_black_24dp, "Input Item"),
+            ToolFragment_Adapter.ToolItem(R.drawable.plus_black, "")
+    )
+  }
 
   override fun onCreateView(
     inflater: LayoutInflater,
@@ -26,10 +40,30 @@ class HomeFragment : Fragment() {
     //sumber gambar:
     //https://www.booked.net/widgets/weather
     val weatherWidget = root.findViewById<ImageView>(R.id.weather_widget)
+    myRecyclerView = root.findViewById(R.id.shorcut_list)
+    myRecyclerView.setHasFixedSize(true)
     MainActivity.actionBarBack.visibility = View.INVISIBLE
     Glide.with(this).load("https://w.bookcdn.com/weather/picture/3_42375_1_1_137AE9_160_ffffff_333333_08488D_1_ffffff_333333_0_6.png?scode=124&domid=w209&anc_id=88042")
       .fitCenter().into(weatherWidget)
 
     return root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    SetAdapter()
+    myRecyclerView.isLayoutFrozen = true //disable scrool
+  }
+
+  fun SetAdapter(){
+    myRecyclerView.layoutManager = GridLayoutManager(getActivity(), 3)
+    val ListAdapter = ToolFragment_Adapter(items)
+    myRecyclerView.adapter = ListAdapter
+
+    ListAdapter.setOnItemClickCallback(object : ToolFragment_Adapter.OnItemClickCallback {
+      override fun onItemClicked(data: ToolFragment_Adapter.ToolItem) {
+        //todo, gak bisa di klik waktu di tes pake toast. Tapi kayaknya lebih baik gak pae ini tapi ganti imageview jadi imagebutton
+      }
+    })
   }
 }
